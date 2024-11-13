ecdf9941b96dcaa290f56fb08eda3f20a868ca92
Asif Ahammed <asifahammed359@gmail.com>
2024-11-14 01:18:15 +0600

    subtraction

---
+++ Lab_mid/subtraction.asm
@@ -0,0 +1,39 @@
+.model small
+.stack 100h
+.data 
+    a db ?
+    b db ?
+.code 
+
+main proc 
+    mov ax, @data 
+    mov ds, ax 
+
+    mov ah, 1 
+    int 21h 
+    mov a, al 
+
+    mov ah, 1 
+    int 21h 
+    mov b, al 
+
+    mov ah, 2
+    mov dl, 0ah
+    int 21h
+    mov dl, 0dh
+    int 21h
+
+    mov cl, a 
+    sub cl, b
+    add cl, 48 ; convert ascii to number
+
+    mov ah, 2
+    mov dl, cl 
+    int 21h  
+
+
+    exit:
+    mov ah, 4ch
+    int 21h
+    main endp
+end main
