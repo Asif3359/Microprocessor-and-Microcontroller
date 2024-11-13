ca7037afa0af089c83d4b8e8d3edbce2a5405b19
Asif Ahammed <asifahammed359@gmail.com>
2024-11-13 19:20:29 +0600

    new line print

--- Lab_mid/simple_input_output.asm
+++ Lab_mid/simple_input_output.asm
@@ -7,10 +7,17 @@ main proc
 
     mov ah, 1 
     int 21h
-    mov bl, al 
+    mov bl, al ; default input al
+
+    ; new line 
+    mov AH, 2
+    mov dl, 0AH
+    int 21H 
+    mov dl, 0DH
+    int 21H 
 
     mov ah, 2 
-    mov dl, bl 
+    mov dl, bl ; dafult output bl
     int 21h 
 
     exit:
